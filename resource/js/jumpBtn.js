6c8f7b06006ce6d9612460458aae3fcf41f3f7b1
JacobCEU <124430062+JacobCEU@users.noreply.github.com>
2023-10-06 16:54:38 +0800

    Fixed CrsAdm, JumpButton
    Added JumpButton

--- resource/js/jumpBtn.js
+++ resource/js/jumpBtn.js
@@ -1,12 +1,10 @@
-// Get the button
-var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+window.onscroll = function() {
   scrollFunction();
 };
 
 function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollToTopBtn.style.display = "block";
   } else {
@@ -14,8 +12,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function scrollToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
