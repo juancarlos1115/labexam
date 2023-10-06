9ce200946316961bdf581354bdc8400f45182296
Joykobe <115333656+Joykobe@users.noreply.github.com>
2023-10-06 15:37:41 +0800

    announcement ko huy 5

--- resource/js/loader.js
+++ resource/js/loader.js
@@ -1,6 +1,5 @@
-document.querySelector('.spinner-border').classList.add('d-block');
 
-// Hide the loader (you can replace this with your actual loading logic)
-setTimeout(function () {
-    document.querySelector('.spinner-border').classList.remove('d-block');
-}, 3000); // Hides the loader after 3 seconds (replace with your actual loading logic)
+window.addEventListener('load', function () {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'none'; // Hide the loader
+});
